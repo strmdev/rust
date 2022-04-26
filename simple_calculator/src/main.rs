60fb1d44633504bbf93e10fd0121c9e28b047cea
mstrihovanecz <m.strihovanecz@gmail.com>
2022-04-26 20:59:52 +0200

    Modified main and calculate functions

--- simple_calculator/src/main.rs
+++ simple_calculator/src/main.rs
@@ -3,7 +3,8 @@ use std::io;
 struct Calculator;
 impl Calculator {
     fn read_data(&self, message: &str, error_message: &str) -> String {
-        let mut data = String::new();    
+        let mut data = String::new();  
+          
         println!("{}", message);
         io::stdin()
             .read_line(&mut data)
@@ -12,14 +13,16 @@ impl Calculator {
         data.trim().to_string()
     }
 
-    fn calculate(&self, number_1: &String, number_2: &String, operator: &String) -> i32 {
-        let mut result: i32 = 0;
+    fn calculate(&self, number_1: &String, number_2: &String, operator: &String) -> f32 {
+        let mut result: f32 = 0.0;
+        let converted_number_1: f32 = number_1.trim().parse::<f32>().unwrap();
+        let converted_number_2: f32 = number_2.trim().parse::<f32>().unwrap();
     
         match operator.trim() {
-            "+" => result = number_1.trim().parse::<i32>().unwrap() + number_2.trim().parse::<i32>().unwrap(),
-            "-" => result = number_1.trim().parse::<i32>().unwrap() - number_2.trim().parse::<i32>().unwrap(),
-            "*" => result = number_1.trim().parse::<i32>().unwrap() * number_2.trim().parse::<i32>().unwrap(),
-            "/" => result = number_1.trim().parse::<i32>().unwrap() / number_2.trim().parse::<i32>().unwrap(),
+            "+" => result = converted_number_1 + converted_number_2,
+            "-" => result = converted_number_1 - converted_number_2,
+            "*" => result = converted_number_1 * converted_number_2,
+            "/" => result = converted_number_1 / converted_number_2,
             _   => println!("Az operator csak '+', '-', '*', '/' lehet!"),
         }
     
@@ -29,10 +32,10 @@ impl Calculator {
 
 fn main() {
     let calculator = Calculator;
-    let number_1: String = calculator.read_data("Elso szam:", "Hiba tortent a beolvasas soran!");
-    let number_2: String = calculator.read_data("Masodik szam:", "Hiba tortent a beolvasas soran!");
-    let operator: String = calculator.read_data("Operator ('+', '-', '*', '/'):", "Hiba tortent a beolvasas soran!");
+    let number_1: String = calculator.read_data("Elso szam:", "Hiba tortent az elso szam beolvasasa soran!");
+    let number_2: String = calculator.read_data("Masodik szam:", "Hiba tortent a masodik szam beolvasasa soran!");
+    let operator: String = calculator.read_data("Operator ('+', '-', '*', '/'):", "Hiba tortent az operator beolvasasa soran!");
 
-    let result: i32 = calculator.calculate(&number_1, &number_2, &operator);
+    let result: f32 = calculator.calculate(&number_1, &number_2, &operator);
     println!("{} {} {} = {}", number_1, operator, number_2, result);
 }
