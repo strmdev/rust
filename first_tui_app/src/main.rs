f7b96d0bcffd2d8092df9f06f6dd990f88537976
mstrihovanecz <m.strihovanecz@gmail.com>
2022-06-02 19:08:29 +0200

    Add population mortality probability

--- first_tui_app/src/main.rs
+++ first_tui_app/src/main.rs
@@ -59,31 +59,83 @@ const MAX_COLUMN_NUM: usize = 155;
 const MAX_ROW_NUM: usize    = 8;   
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let fox_birth_probability: f64        = read_data("Rokak szuletesenek valoszinusege (0.0..1.0):", "Hiba tortent a beolvasas soran!");
+    let fox_mortality_probability: f64    = read_data("Rokak halalozasanak valoszinusege (0.0..1.0):", "Hiba tortent a beolvasas soran!");
+    let rabbit_mortality_probability: f64 = read_data("Nyulak halalozasanak valoszinusege (0.0..1.0):", "Hiba tortent a beolvasas soran!");
+
     enable_raw_mode()?;
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
+
+    let backend      = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     
-    let run_app_result = run_app(&mut terminal);
+    let run_app_result = run_app(&mut terminal, fox_birth_probability, fox_mortality_probability, rabbit_mortality_probability);
 
     disable_raw_mode()?;
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     terminal.show_cursor()?;
 
     if let Err(error_message) = run_app_result {
-        println!("{:?}", error_message)
+        println!("Hiba: {:?}", error_message)
+    } else {
+        println!("\nSzimulacio vege...");
     }
 
     Ok(())
 }
 
-fn generate_population(population: &mut Vec<Vec<&str>>) {
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, fox_birth_probability: f64, fox_mortality_probability: f64, rabbit_mortality_probability: f64) -> io::Result<()> {
+    let mut population: Vec<Vec<&str>> = vec![vec![" ";MAX_COLUMN_NUM];MAX_ROW_NUM];    
+    generate_population(&mut population, fox_birth_probability);
+
+    loop {
+        animal_mortality(&mut population, fox_mortality_probability, rabbit_mortality_probability);
+        fox_eating(&mut population);
+
+        terminal.draw(|frame| ui(frame, &population))?;
+
+        if crossterm::event::poll(Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()? {
+                if key.code == KeyCode::Char('q') || key.code == KeyCode::Char('Q') {
+                    return Ok(());
+                }
+                if key.code == KeyCode::Char('s') || key.code == KeyCode::Char('S') {
+                    event::read()?; 
+                }
+            }
+        }
+    }
+}
+
+fn read_data(message: &str, error_message: &str) -> f64 {
+    loop {
+        let mut data: String = String::new();
+    
+        println!("{}", message);
+        io::stdin()
+           .read_line(&mut data)
+           .expect(error_message);
+        
+        let value: f64 = data.trim()
+                             .parse::<f64>()
+                             .unwrap();
+
+        if value >= 0.0 && value <= 1.0  {
+            break value
+        } else {
+            println!("Az ertek csak 0.0..1.0 kozott lehet!")
+        }
+    }
+}
+
+fn generate_population(population: &mut Vec<Vec<&str>>, fox_birth_probability: f64) {
     for simulation_space_row in population.iter_mut() {
         for simulation_space_cell in simulation_space_row.iter_mut() {
             let birth_probability: f64 = rand::thread_rng()
                                               .gen_range(0.0..1.0);
-            if birth_probability >= 0.5 {
+
+            if birth_probability < fox_birth_probability {
                 *simulation_space_cell = "R";
             } else {
                 *simulation_space_cell = "N";
@@ -195,24 +247,21 @@ fn fox_eating(population: &mut Vec<Vec<&str>>) {
     }
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
-    let mut population: Vec<Vec<&str>> = vec![vec![" ";MAX_COLUMN_NUM];MAX_ROW_NUM];    
-    generate_population(&mut population);
+fn animal_mortality(population: &mut Vec<Vec<&str>>, fox_mortality_probability: f64, rabbit_mortality_probability: f64) {
+    let x_position: usize = rand::thread_rng()
+                                 .gen_range(0..155);
+    let y_position: usize = rand::thread_rng()
+                                 .gen_range(0..8);
 
-    loop {
-        fox_eating(&mut population);
-        terminal.draw(|frame| ui(frame, &population))?;
-
-        if crossterm::event::poll(Duration::from_millis(100))? {
-            if let Event::Key(key) = event::read()? {
-                if (key.code == KeyCode::Char('q')) || (key.code == KeyCode::Char('Q')) {
-                    return Ok(());
-                }
-                if (key.code == KeyCode::Char('s')) || (key.code == KeyCode::Char('S')) {
-                    event::read()?; 
-                }
-            }
-        }
+    let rabbit_mortality: f64 = rand::thread_rng()
+                                     .gen_range(0.0..1.0);
+    let fox_mortality: f64    = rand::thread_rng()
+                                     .gen_range(0.0..1.0);
+    
+    if population[y_position][x_position] == "N" && rabbit_mortality < rabbit_mortality_probability {
+        population[y_position][x_position] = "F";
+    } else if population[y_position][x_position] == "R" && fox_mortality < fox_mortality_probability {
+        population[y_position][x_position] = "F";
     }
 }
 
@@ -231,7 +280,7 @@ fn get_symbol_count(population: &Vec<Vec<&str>>, symbol: &str) -> u64 {
     symbol_count
 }
 
-fn print_animal(simulation_space_cell: usize, simulation_space_row: usize, value: &str) {
+fn print_symbol(simulation_space_cell: usize, simulation_space_row: usize, value: &str) {
     if value == "R" {
         execute!(io::stdout(),
                  cursor::MoveTo((simulation_space_cell + 1) as u16, (simulation_space_row + 1) as u16),
@@ -241,6 +290,10 @@ fn print_animal(simulation_space_cell: usize, simulation_space_row: usize, value
         execute!(io::stdout(),
                  cursor::MoveTo((simulation_space_cell + 1) as u16, (simulation_space_row + 1) as u16),
                  PrintStyledContent(value.with(crossterm::style::Color::Grey))).ok();
+    } else if value == "F" {
+        execute!(io::stdout(),
+                 cursor::MoveTo((simulation_space_cell + 1) as u16, (simulation_space_row + 1) as u16),
+                 PrintStyledContent(value.with(crossterm::style::Color::Green))).ok();
     } else if value == " " {
         execute!(io::stdout(),
                  cursor::MoveTo((simulation_space_cell + 1) as u16, (simulation_space_row + 1) as u16),
@@ -253,7 +306,7 @@ fn show_population(population: &Vec<Vec<&str>>) {
     for simulation_space_row in 0..MAX_ROW_NUM {
         for simulation_space_cell in 0..MAX_COLUMN_NUM {
             value = population[simulation_space_row][simulation_space_cell];
-            print_animal(simulation_space_cell, simulation_space_row, value);     
+            print_symbol(simulation_space_cell, simulation_space_row, value);     
         }
     }
 }
@@ -262,7 +315,7 @@ fn clear_simulation_space() {
     let clear_symbol = " ";
     for simulation_space_row in 0..MAX_ROW_NUM {
         for simulation_space_cell in 0..MAX_COLUMN_NUM {
-            print_animal(simulation_space_cell, simulation_space_row, clear_symbol);     
+            print_symbol(simulation_space_cell, simulation_space_row, clear_symbol);     
         }
     }    
 }
@@ -323,7 +376,7 @@ fn ui<B: Backend>(frame: &mut Frame<B>, population: &Vec<Vec<&str>>) {
                                                                                .fg(Color::LightGreen))
                                               )
                                               .data(fox_rabbit_data)
-                                              .bar_width(18)
+                                              .bar_width(22)
                                               .bar_gap(16)
                                               .bar_style(Style::default()
                                                                .fg(Color::LightMagenta))
@@ -335,7 +388,7 @@ fn ui<B: Backend>(frame: &mut Frame<B>, population: &Vec<Vec<&str>>) {
     frame.render_widget(fox_rabbit_chart, chart_screen[0]);
 
     let population_count_diff: u64 = count_fox + count_rabbit;
-    let population_count: u64      = population_count_diff + get_symbol_count(&population, " ");
+    let population_count: u64      = population_count_diff + get_symbol_count(&population, " ") + get_symbol_count(&population, "F");
     let population_data: &[(& str, u64)] = &[
                                                 ("Populacio indulo szama", population_count),
                                                 ("Populacio akt. szama", population_count_diff)
